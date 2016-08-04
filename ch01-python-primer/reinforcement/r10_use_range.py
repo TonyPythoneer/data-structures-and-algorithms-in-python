1fc44a0019f05a7894419a56b582b26655574543
TonyPythoneer <tonypythoneer@gmail.com>
2016-08-04 10:43:54 +0800

    [ENHANCE] Complete r10.py

--- ch01-python-primer/reinforcement/r10_use_range.py
+++ ch01-python-primer/reinforcement/r10_use_range.py
@@ -9,13 +9,17 @@ to produce a range with values 8, 6, 4, 2, 0, −2, −4, −6, −8?
 import unittest
 
 
-def func(data):
+def func():
     return list(xrange(8, -10, -2))
 
 
 class Test(unittest.TestCase):
     def setUp(self):
         self.func = func
+
+    def test_first_case(self):
+        expect_result = [8, 6, 4, 2, 0, -2, -4, -6, -8]
+        self.assertEqual(expect_result, self.func())
 
 
 if __name__ == '__main__':
