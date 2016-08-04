e45277b72e77340d1fee04a9f89a18f67118427d
TonyPythoneer <tonypythoneer@gmail.com>
2016-08-04 10:12:02 +0800

    [ENHANCE] Complete r8

--- ch01-python-primer/reinforcement/r8.py
+++ ch01-python-primer/reinforcement/r8.py
@@ -14,20 +14,23 @@ import unittest
 def func(seq, index):
     if index > 0:
         raise IndexError("The index must be negative")
-    return seq[-index - 1]
+    return seq[len(seq) + index]
 
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.seq = [1 , 2, 3, 4, 5]
+        self.seq = [1, 2, 3, 4, 5]
         self.func = func
 
     def test_first_case(self):
-        self.
-        self.func(self.seq, -1)
+        expected_result = self.seq[-1]
+        func_result = self.func(self.seq, -1)
+        self.assertEqual(expected_result, func_result)
 
     def test_second_case(self):
-        pass
+        expected_result = self.seq[-2]
+        func_result = self.func(self.seq, -2)
+        self.assertEqual(expected_result, func_result)
 
 
 if __name__ == '__main__':
