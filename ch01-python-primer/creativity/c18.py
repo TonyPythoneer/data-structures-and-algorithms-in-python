ee7faf81c3ac08da8ce4c54128ff915b1bf66065
TonyPythoneer <tonypythoneer@gmail.com>
2016-08-11 18:33:53 +0800

    [ENHANCE] Complete c18.py

--- ch01-python-primer/creativity/c18.py
+++ ch01-python-primer/creativity/c18.py
@@ -7,13 +7,35 @@
 import unittest
 
 
-def func(data):
-    pass
+def func():
+    seq = [0]
+    for i in range(1, 10):
+        seq.append(seq[-1] + 2 * i)
+    return seq
 
 
-class Test(unittest.TestCase):
+def func2():
+    seq = []
+    last_num = 0
+    for i in range(0, 10):
+        seq.append(last_num + 2 * i)
+        last_num = seq[i]
+    return seq
+
+
+class TestFunc(unittest.TestCase):
+    expected_output = [0, 2, 6, 12, 20, 30, 42, 56, 72, 90]
+
     def setUp(self):
         self.func = func
+
+    def test_first_case(self):
+        self.assertEqual(self.func(), self.expected_output)
+
+
+class TestFunc2(unittest.TestCase):
+    def setUp(self):
+        self.func = func2
 
 
 if __name__ == '__main__':
